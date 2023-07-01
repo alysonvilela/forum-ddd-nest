9f4a605706d47497568b7e24fb73d20a141623a1
Alyson Vilela <ctt.alysonvilela@gmail.com>
2023-07-01 17:00:53 -0300

    feat: add create method to entities

--- src/domain/core/entities/unique-entity-id.ts
+++ src/domain/core/entities/unique-entity-id.ts
@@ -1,17 +1,17 @@
 import { v4 as uuidV4 } from "uuid";
 
 export class UniqueEntityID {
-private value: string
+  private value: string;
 
-toString() {
-   return this.value
-}
+  toString() {
+    return this.value;
+  }
 
-toValue() {
-   return this.value
-}
+  toValue() {
+    return this.value;
+  }
 
-constructor(value?: string) {
-   this.value = value ?? uuidV4()
-}
+  constructor(value?: string) {
+    this.value = value ?? uuidV4();
+  }
 }
