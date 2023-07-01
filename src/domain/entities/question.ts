d1164b066365b42a8634f74fe50174b887f00710
Alyson Vilela <ctt.alysonvilela@gmail.com>
2023-07-01 16:38:21 -0300

    refactor: use unique entities ids

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,10 +1,12 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../core/entities/entity";
+import { UniqueEntityID } from "../core/entities/unique-entity-id";
 
 interface QuestionProps {
+   authorId: UniqueEntityID;
+   bestAnswerId?: UniqueEntityID;
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
 }
 export class Question extends Entity<QuestionProps> {}
