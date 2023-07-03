f83e44669706e521983dc58015c720d85ee885b3
Alyson Vilela <ctt.alysonvilela@gmail.com>
2023-07-03 14:27:33 -0300

    feat: add delete answer use case

---
+++ src/domain/forum/application/use-cases/delete-answer.specs.ts
@@ -0,0 +1,48 @@
+import { InMemoryAnswersRepository } from '@/test/repositories/answers-repository-in-memory'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { makeAnswer } from '@/test/factories/make-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+
+describe('Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('should be able to delete an answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+    inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      answerId: 'answer-1',
+    })
+
+    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete an answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+    inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(
+      sut.execute({
+        authorId: 'author-2',
+        answerId: 'answer-1',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
--- src/test/repositories/answers-repository-in-memory.ts
+++ src/test/repositories/answers-repository-in-memory.ts
@@ -3,7 +3,22 @@ import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
 export class InMemoryAnswersRepository implements AnswersRepository {
   public items: Answer[] = []
+
   async create(answer: Answer) {
     this.items.push(answer)
+  }
+
+  async delete(question: Answer) {
+    const itemIdx = this.items.findIndex((item) => item.id === question.id)
+    this.items.splice(itemIdx, 1)
+  }
+
+  async findById(id: string) {
+    const answer = this.items.find((answer) => answer.id.toString() === id)
+    if (!answer) {
+      return null
+    }
+
+    return answer
   }
 }
