b052732820f3fcb2c990f3ec5b68429d5af40f4a
Alyson Vilela <ctt.alysonvilela@gmail.com>
2023-07-04 23:08:51 -0300

    feat: add fetch answer comments usecase

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -0,0 +1,58 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswerCommentsRepository } from '@/test/repositories/answer-comments-repository-in-memory'
+import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
+import { makeAnswerComment } from '@/test/factories/make-answer-comment'
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let sut: FetchAnswerCommentsUseCase
+
+describe('Fetch Answer Comments', () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
+  })
+
+  it('should be able to fetch answer comments', async () => {
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+    await inMemoryAnswerCommentsRepository.create(
+      makeAnswerComment({
+        answerId: new UniqueEntityID('answer-1'),
+      }),
+    )
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 1,
+    })
+
+    expect(answerComments).toHaveLength(3)
+  })
+
+  it('should be able to fetch paginated answer comments', async () => {
+    await Promise.all(
+      [...Array(22)].map(
+        async () =>
+          await inMemoryAnswerCommentsRepository.create(
+            makeAnswerComment({
+              answerId: new UniqueEntityID('answer-1'),
+            }),
+          ),
+      ),
+    )
+
+    const { answerComments } = await sut.execute({
+      answerId: 'answer-1',
+      page: 2,
+    })
+
+    expect(answerComments).toHaveLength(2)
+  })
+})
